4182235ad0cd75162bebc6f8da1edec7baf41048
Ilya Knyazhansky <ilya@appscloud.io>
2018-01-30 15:36:02 +0200

    add projects

--- server.js
+++ server.js
@@ -3,33 +3,28 @@ const path = require('path');
 const hbs = require("hbs");
 
 const port = process.env.PORT || 3000;
+const app = express();
 
 hbs.registerPartials(path.join(__dirname,"views","partials"));
-
-var app = express();
+hbs.registerHelper('currentYear', () => new Date().getFullYear());
 
 app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname,"public")));
 
-/*
-app.use((req, resp, next) => {
-	resp.render("down", {
-		now: new Date().toString()
-	});
-});
-*/
-
 app.get('/', (req, resp) => {
 	resp.render("home", {
-		welcomeMessage: "Hello You",
-		obj: {a:"a"},
-		currentYear: new Date().getFullYear()
+		welcomeMessage: "Welcome HOME!"
 	});
 });
 
 app.get('/about', (req, resp) => {
 	resp.render("about.hbs");
 });
+
+app.get('/projects', (req, resp) => {
+	resp.render("projects.hbs");
+});
+
 
 app.listen(port, () => console.log("Starting on port", port));
